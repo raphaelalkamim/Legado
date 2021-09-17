738ead075da83f5d954f1f27cd9ff2f9d1fbe664
raphaelalkamim <83611337+raphaelalkamim@users.noreply.github.com>
2021-09-17 17:14:34 -0300

    complementar storyboard

---
+++ olds/Album/AlbumGridViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AlbumGridViewController.swift
+//  olds
+//
+//  Created by Raphael Alkamim on 16/09/21.
+//
+
+import UIKit
+
+class AlbumGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- olds/Album/AlbumViewController.swift
+++ olds/Album/AlbumViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AlbumViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
