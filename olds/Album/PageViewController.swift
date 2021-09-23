dc44580cd197d84c610cd71324392abb03ee9449
caroltaus <carolinetaus@hotmail.com>
2021-09-23 19:02:45 -0300

    next page implementation

--- olds/Album/PageViewController.swift
+++ olds/Album/PageViewController.swift
@@ -8,22 +8,32 @@
 import UIKit
 
 class PageViewController: UIViewController {
+    var page: Page?
+    var date: Date?
+    var album: Album?
+    var pageIndex: Int?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    func convertDate(date: Date) -> String {
+        self.date = date
+        let formatter = DateFormatter()
+        formatter.dateStyle = DateFormatter.Style.full
+        let stringDate = formatter.string(from: date)
+        return stringDate
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        date = page?.pageDate
+        
+        title = convertDate(date: (page?.pageDate)!)
+        print("title: ",title)
+        print("AAAAA PAGINA", page)
     }
-    */
+    func changeAlbum(album: Album?) {
+        self.album = album
+    }
+
+    
 
 }
