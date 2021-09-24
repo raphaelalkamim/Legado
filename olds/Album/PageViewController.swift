b6fdc7e6945f3c2c42fa8d87559bd5314713398f
caroltaus <carolinetaus@hotmail.com>
2021-09-24 16:20:25 -0300

    início da implementação de paginaçao

--- olds/Album/PageViewController.swift
+++ olds/Album/PageViewController.swift
@@ -14,6 +14,28 @@ class PageViewController: UIViewController {
     var pageIndex: Int?
 
     
+    @IBOutlet weak var previousButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+   
+    
+    @IBAction func goNext(_ sender: Any) {
+        print("fui pra next")
+        if let vc = storyboard?.instantiateViewController(identifier: "pageView") as? PageViewController {
+            vc.changeAlbum(album: album)
+            let index = pageIndex!+1
+            vc.page = album?.pages?.allObjects[index] as? Page
+            vc.pageIndex = pageIndex!+1
+            navigationController?.pushViewController(vc, animated: true)
+            
+        }
+    }
+    
+    @IBAction func goPrevious(_ sender: Any) {
+        print("previ")
+    }
+    
+    
+    
     func convertDate(date: Date) -> String {
         self.date = date
         let formatter = DateFormatter()
@@ -29,9 +51,26 @@ class PageViewController: UIViewController {
         title = convertDate(date: (page?.pageDate)!)
         print("title: ",title)
         print("AAAAA PAGINA", page)
+        
+        let numberOfPages = album?.pages?.count
+        print("NUMPAGES",numberOfPages)
+        print("PAGESINDEX",pageIndex)
+        print(album)
+        print(album?.pages)
+        if pageIndex == (numberOfPages! - 1) {
+            nextButton.isHidden = true
+
+        }
+        
+        
     }
     func changeAlbum(album: Album?) {
         self.album = album
+    }
+    
+   
+    @IBAction func goBackToAlbums(_ sender: UIBarButtonItem) {
+        self.navigationController?.popToRootViewController(animated: true)
     }
 
     
